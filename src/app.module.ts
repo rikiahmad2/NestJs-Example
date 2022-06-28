098b1db1b1ee2a909b070c56308e3bc8c83cc9af
rikiahmad2 <ahmadriki9512@gmail.com>
2022-06-28 14:07:36 +0700

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port: number = 3000;
+  const host: string = 'localhost';
+
+  /**
+   * Enable Helmet
+   * Helmet helps you secure your Express apps by setting various HTTP headers.
+   * https://github.com/helmetjs/helmet#how-it-works
+   */
+  app.use(helmet());
+
+  /**
+   * Enable Cors
+   * https://docs.nestjs.com/security/cors
+   */
+  app.enableCors();
+
+  /**
+   * Swagger Config
+   * https://docs.nestjs.com/openapi/introduction
+   */
+  const docConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription(
+      'Riki Ahmad Practice Serverless API with NestJS, TypeScript, and AWS Lambda',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port, host, () => {
+    console.log('[SERVICE]', `${host}:${port}`);
+  });
 }
 bootstrap();
